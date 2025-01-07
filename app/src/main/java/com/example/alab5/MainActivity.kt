c4510bc1c8628a313ebd17592c0a7c05a2c1b750
LastProgram <145275888+LastProgram@users.noreply.github.com>
2025-01-07 16:37:55 +0700

    main activity class

--- app/src/main/java/com/example/alab5/MainActivity.kt
+++ app/src/main/java/com/example/alab5/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.alab5
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var radioButton1: RadioButton
+    private lateinit var radioButton2: RadioButton
+    private lateinit var radioButton3: RadioButton
+    private lateinit var buttonOk: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        radioButton1 = findViewById(R.id.radioButton1)
+        radioButton2 = findViewById(R.id.radioButton2)
+        radioButton3 = findViewById(R.id.radioButton3)
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            radioButton1.isSelected = checkedId == radioButton1.id
+            radioButton2.isSelected = checkedId == radioButton2.id
+            radioButton3.isSelected = checkedId == radioButton3.id
         }
+
+        buttonOk.setOnClickListener { _ ->
+
+        }
+
     }
 }
